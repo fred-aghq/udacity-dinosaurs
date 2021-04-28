7249c2fa44331ab927f004bcd85e31a435f4f129
Fred Spooner <fred@aghq.uk>
2021-04-28 16:21:06 +0100

    chore: autoformat index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,57 @@
 import './app.css';
-import submitHandler from './submitHandler.js';
 import dinoJson from './dino.json';
 import DinoFactory from './DinoFactory.js';
+import submitHandler from './submitHandler.js';
 
 console.log(dinoJson.Dinos);
 
-    // Create Dino Constructor
+// Create Dino Constructor
 
 
 // @TODO: remove this test/prototype code.
-dinoJson.Dinos.forEach(function(v) {
-    const dino = DinoFactory(v);
-    console.log(dino);
-    console.log(dino.getSpecies());
-})
-
-const Human = function() {}
-
-    // Create Dino Objects
+const dinoObjects = dinoJson.Dinos.map(function (v) {
+  const dino = DinoFactory(v);
+  return dino;
+});
 
 
-    // Create Human Object
 
-    // Use IIFE to get human data from form
-    // @see submitHandler.js
-    const form = document.getElementById('dino-compare');
-    form.addEventListener('submit', submitHandler);
-    form.addEventListener('submit', function(event) {
-        event.target.remove();
-    });
+// function Human(name, height, weight, diet){
+//     function getName() { return this.name; }
+// }
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Objects
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Human Object
+// const human = new Human('fred', 72, 180, 'omnivor');
+// console.log(human.getName());
 
-    // Remove form from screen
+// Use IIFE to get human data from form
+// @see submitHandler.js
+const form = document.getElementById('dino-compare');
+form.addEventListener('submit', submitHandler);
+form.addEventListener('submit', function (event) {
+  event.target.remove();
+});
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
